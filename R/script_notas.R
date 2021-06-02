b94f5be1e8e9078cdfe3c16cdbeefaf2e84cc870
Ícaro Vidal Freire <icarofreire7@gmail.com>
2021-06-02 14:17:38 -0300

    adicionando as medias final para analise

--- R/script_notas.R
+++ R/script_notas.R
@@ -33,3 +33,39 @@ nota_parcial <- nota %>%
   select(matricula, prova_01)
 
 nota_parcial
+
+# Criando função de arredondamento
+
+nota_licao <- function(n){
+  round(n/23, 1)
+}
+
+nota_licao(1970)
+
+# TESTES
+nota_teste <- tribble(
+  ~matricula  ,  ~prova_01, ~atv_01, ~atv_02, ~atv_03, ~atv_04, ~prova_02,
+  "20101415"  ,  42, 58,  54, 45, 50, 35,
+  "201611179" ,  52, 87, 100, 47, 65, 90,
+  "201111593" ,  44, 58,  54, 45, 50, 53,
+  "201210054" ,  NA, NA,  NA, NA, NA, NA,
+  "201411224" ,  24, 87, 100, 47, 65, 78,
+  "2017204574", 100, 95,  94, 80, 88, 98,
+  "2017204500", 100, 95,  94, 80, 88, 97,
+  "20101448"  ,  40, 58,  54, 45, 50, NA,
+  "201611170" ,  31, 87, 100, 47, 65, 90
+)
+
+
+modi <- nota_teste %>%
+  pivot_longer(
+    !matricula,
+    names_to = "todas_ativ",
+    values_to = "notas"
+  )
+
+modi %>%
+  group_by(matricula, todas_ativ) %>%
+  summarise(
+    media = mean(notas, na.rm = TRUE)
+  )
